19c6831045bf2c53863d673f8d802582df735ea5
github username <github email address>
2019-07-26 15:35:26 -0700

    clicking get searching button redirects from welcome to homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Welcome from "./components/Welcome.js";
+import Homepage from "./components/Homepage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Welcome />
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/welcome" component={() => <Welcome />} />
+      <Route exact path="/search" component={() => <Homepage />} />
+    </Router>
   );
 }
 
---
+++ src/components/Homepage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <h1> this is the homepage where they will search images</h1>
+      </div>
+    );
+  }
+}
